97abc976ef3d05330f20094cdbd4e4b9e5bfa3d8
Ben Zwick <benzwick@gmail.com>
2023-05-03 15:35:41 +0800

    Create dti.sh example

---
+++ examples/dti.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Create a mesh from a brain label map with diffusion tensors
+# The data for this example is available at https://zenodo.org/record/7687631
+# For further details, see the following publications:
+# - Zwick BF, Bourantas GC, Safdar S, Joldes GR, Hyde DE, Warfield SK, Wittek A, Miller K. Patient-specific solution of the electrocorticography forward problem in deforming brain. NeuroImage. 2022;263:119649, DOI: 10.1016/j.neuroimage.2022.119649.
+# - Zwick BF, Safdar S, Bourantas GC, Joldes GR, Hyde DE, Warfield SK, Wittek A, Miller K. Image data and computational grids for computing brain shift and solving the electrocorticography forward problem. Data Brief. 2023;48:109122, DOI: 10.1016/j.dib.2023.109122.
+mvox --input-attributes labelmap.nrrd --input-tensors cond.nrrd --output-mesh mesh.mesh.gz --output-tensors cond.gf.gz
