2648f9b9f0965d70684356c908e0007a5e5d77c4
Ben Zwick <benzwick@gmail.com>
2023-05-03 15:07:11 +0800

    Compress boxmesh.sh example mesh

--- examples/boxmesh.sh
+++ examples/boxmesh.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Create a boxmesh using the dimensions of the input image
 wget https://github.com/mhalle/spl-brain-atlas/raw/master/slicer/volumes/imaging/A1_grayT1-1mm_resample.nrrd
-mvox -box -imask A1_grayT1-1mm_resample.nrrd -omesh mesh.mesh
+mvox -box -imask A1_grayT1-1mm_resample.nrrd -omesh mesh.mesh.gz
