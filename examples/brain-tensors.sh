7bcc93b21c2a4dd4175abd84f2a5c34cf8641a89
Ben Zwick <benzwick@gmail.com>
2026-02-14 15:59:07 +0800

    Fix missing --output-tensors flag in brain-tensors VTK command

--- examples/brain-tensors.sh
+++ examples/brain-tensors.sh
@@ -5,4 +5,4 @@
 # - Zwick BF, Bourantas GC, Safdar S, Joldes GR, Hyde DE, Warfield SK, Wittek A, Miller K. Patient-specific solution of the electrocorticography forward problem in deforming brain. NeuroImage. 2022;263:119649, DOI: 10.1016/j.neuroimage.2022.119649.
 # - Zwick BF, Safdar S, Bourantas GC, Joldes GR, Hyde DE, Warfield SK, Wittek A, Miller K. Image data and computational grids for computing brain shift and solving the electrocorticography forward problem. Data Brief. 2023;48:109122, DOI: 10.1016/j.dib.2023.109122.
 mvox --input-attributes labelmap.nrrd --input-tensors cond.nrrd --output-mesh mesh.mesh.gz --output-tensors cond.gf.gz
-mvox --input-attributes labelmap.nrrd --input-tensors cond.nrrd --output-mesh brain.vtk
+mvox --input-attributes labelmap.nrrd --input-tensors cond.nrrd --output-mesh brain.vtk --output-tensors cond-brain.gf.gz
